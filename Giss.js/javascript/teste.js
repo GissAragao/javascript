c11f511bf2f454b42392cce8d0098dc8f07854a7
Gislayne Aragão <112116124+GissAragao@users.noreply.github.com>
2022-11-10 15:53:23 -0300

    Add files via upload

---
+++ Giss.js/javascript/teste.js
@@ -0,0 +1,9 @@
+
+var nome = window.prompt ("qual é o seu nome?");
+var genero  = window.prompt ("qual genero de filme você gosta?");
+
+
+document.write("Olá," + nome + ",que gosta de filme..." + genero + ".Bom gosto!")
+
+
+
---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+// variável é um local de armazenamento de informações
+
+var filme = ("Qual é o seu número?");
+var atorPrincipal =("Chris Evans");
+var alturaEmCm = 183;
+var idade = 41;
+
+document.write(filme);
+document.write(alturaEmCm);
+document.write(atorPrincipal);
+
+console.log(idade);
+
+var num1 = 50;
+var num2 = 10;
+var num3 = 2;
+var resultado = (num1+num2)* num3;
+  
+console.log  = (resultado)
+
+// soma +
+// subtração 
+// multiplicação
+// divisão /
+
+//5 > number 
+
+//28 > number 
+
+//-75 > number
+
+//-32,4 > number
+
+//0.1 > number 
+
+//decimal > ponto flutuante > 12. . . . . . . . . . . 12,8 . 13
+ 
+//integer int 
+
+//float double 
+ 
+//"ppppppp" > conjunto de caracteres > string 
+
+//"sabão" >
+
+//'quarenta' >
+
+//true
+
+//false/
